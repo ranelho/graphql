686c62e47f2e9aae275713b85e7640f14ca65f4b
ranelho <ranelhol@gmail.com>
2025-08-04 16:51:10 -0300

    Fix: Corrigir carregamento de dados e alterar porta para 8085
    - Separar INSERTs de autores em comandos individuais no import.sql
    - Adicionar IDs específicos para autores para manter integridade referencial
    - Alterar porta do servidor de 8080 para 8085 no application.yml
    - Resolver violação de integridade referencial no banco H2

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,25 +1,23 @@
 -- Script SQL para pré-carregar dados no banco H2
 -- Este arquivo será executado automaticamente pelo Spring Boot na inicialização
 
--- Inserir autores
-INSERT INTO author (first_name, last_name, email) VALUES 
-('George', 'Orwell', 'george.orwell@email.com'),
-('J.K.', 'Rowling', 'jk.rowling@email.com'),
-('Stephen', 'King', 'stephen.king@email.com'),
-('Agatha', 'Christie', 'agatha.christie@email.com'),
-('Isaac', 'Asimov', 'isaac.asimov@email.com');
+-- Inserir autores primeiro
+INSERT INTO author (id, first_name, last_name, email) VALUES (1, 'George', 'Orwell', 'george.orwell@email.com');
+INSERT INTO author (id, first_name, last_name, email) VALUES (2, 'J.K.', 'Rowling', 'jk.rowling@email.com');
+INSERT INTO author (id, first_name, last_name, email) VALUES (3, 'Stephen', 'King', 'stephen.king@email.com');
+INSERT INTO author (id, first_name, last_name, email) VALUES (4, 'Agatha', 'Christie', 'agatha.christie@email.com');
+INSERT INTO author (id, first_name, last_name, email) VALUES (5, 'Isaac', 'Asimov', 'isaac.asimov@email.com');
 
--- Inserir livros (usando IDs gerados automaticamente para autores)
-INSERT INTO book (title, isbn, page_count, author_id) VALUES 
-('1984', '978-0-452-28423-4', 328, 1),
-('Animal Farm', '978-0-452-28424-1', 112, 1),
-('Harry Potter and the Philosopher''s Stone', '978-0-7475-3269-9', 223, 2),
-('Harry Potter and the Chamber of Secrets', '978-0-7475-3849-3', 251, 2),
-('The Shining', '978-0-385-12167-5', 447, 3),
-('It', '978-0-670-81302-4', 1138, 3),
-('Murder on the Orient Express', '978-0-00-711926-0', 256, 4),
-('The Murder of Roger Ackroyd', '978-0-00-711925-3', 312, 4),
-('Foundation', '978-0-553-29335-0', 244, 5),
-('I, Robot', '978-0-553-29438-8', 253, 5);
+-- Inserir livros após os autores
+INSERT INTO book (title, isbn, page_count, author_id) VALUES ('1984', '978-0-452-28423-4', 328, 1);
+INSERT INTO book (title, isbn, page_count, author_id) VALUES ('Animal Farm', '978-0-452-28424-1', 112, 1);
+INSERT INTO book (title, isbn, page_count, author_id) VALUES ('Harry Potter and the Philosopher''s Stone', '978-0-7475-3269-9', 223, 2);
+INSERT INTO book (title, isbn, page_count, author_id) VALUES ('Harry Potter and the Chamber of Secrets', '978-0-7475-3849-3', 251, 2);
+INSERT INTO book (title, isbn, page_count, author_id) VALUES ('The Shining', '978-0-385-12167-5', 447, 3);
+INSERT INTO book (title, isbn, page_count, author_id) VALUES ('It', '978-0-670-81302-4', 1138, 3);
+INSERT INTO book (title, isbn, page_count, author_id) VALUES ('Murder on the Orient Express', '978-0-00-711926-0', 256, 4);
+INSERT INTO book (title, isbn, page_count, author_id) VALUES ('The Murder of Roger Ackroyd', '978-0-00-711925-3', 312, 4);
+INSERT INTO book (title, isbn, page_count, author_id) VALUES ('Foundation', '978-0-553-29335-0', 244, 5);
+INSERT INTO book (title, isbn, page_count, author_id) VALUES ('I, Robot', '978-0-553-29438-8', 253, 5);
 
 -- Os IDs serão gerados automaticamente pelo Hibernate
