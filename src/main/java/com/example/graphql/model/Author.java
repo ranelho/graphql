411229d99eab8b1dbe4652596bc5d285329bbb2c
ranelho <ranelhol@gmail.com>
2025-08-04 09:23:44 -0300

    Integração do Lombok: - Adicionada dependência do Lombok no pom.xml - Refatoradas entidades Author e Book com anotações @Data, @NoArgsConstructor, @AllArgsConstructor e @Builder - Removidos getters/setters manuais - Código mais limpo e conciso

--- src/main/java/com/example/graphql/model/Author.java
+++ src/main/java/com/example/graphql/model/Author.java
@@ -1,73 +1,38 @@
 package com.example.graphql.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import java.util.List;
 
 @Entity
-@Table(name = "authors")
+@Table(name = "author")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    @Column(name = "first_name", nullable = false)
+    @Column(name = "first_name")
     private String firstName;
     
-    @Column(name = "last_name", nullable = false)
+    @Column(name = "last_name")
     private String lastName;
     
-    @Column(name = "email", unique = true)
     private String email;
     
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Book> books;
 
-    public Author() {}
-
     public Author(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<Book> getBooks() {
-        return books;
-    }
-
-    public void setBooks(List<Book> books) {
-        this.books = books;
     }
 
     public String getFullName() {
--- src/main/java/com/example/graphql/model/Book.java
+++ src/main/java/com/example/graphql/model/Book.java
@@ -1,73 +1,36 @@
 package com.example.graphql.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 @Entity
-@Table(name = "books")
+@Table(name = "book")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    @Column(name = "title", nullable = false)
     private String title;
-    
-    @Column(name = "isbn")
     private String isbn;
     
     @Column(name = "page_count")
-    private int pageCount;
+    private Integer pageCount;
     
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "author_id", nullable = false)
+    @JoinColumn(name = "author_id")
     private Author author;
 
-    public Book() {}
-
-    public Book(String title, String isbn, int pageCount, Author author) {
+    public Book(String title, String isbn, Integer pageCount, Author author) {
         this.title = title;
         this.isbn = isbn;
         this.pageCount = pageCount;
-        this.author = author;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public int getPageCount() {
-        return pageCount;
-    }
-
-    public void setPageCount(int pageCount) {
-        this.pageCount = pageCount;
-    }
-
-    public Author getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(Author author) {
         this.author = author;
     }
 }
